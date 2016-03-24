c2c56cfdc3154dc247ee54c242fc51e71dcc6a2a
robertmackness <robert.mackness.gbr@gmail.com>
2016-03-24 17:57:26 +0000

    Add recursive answers

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    var func = fn.bind(obj);
+    return func();
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
-
-  }
+    var result = [];
+    for (key in obj){
+      if(obj.hasOwnProperty(key)){
+        result.push(key+": "+obj[key]);
+      }
+    }
+    return result;
+  } 
 };
--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,37 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
+    var filesArr = [];
 
+    // Recursively search for correct subdirectory
+    // Call recursive file search once directory hit
+    // Default to root directory 'app'
+    function dirSearch(obj, dir = 'app'){
+      if (obj.dir == dir){
+        recSearch(obj);
+      }else if (obj.files){
+        for(var i=0; i<obj.files.length; i++){
+          if(typeof obj.files[i] == 'object'){
+            dirSearch(obj.files[i], dir);
+          }
+        }
+      }
+    }
+    // Recursively search for files
+    function recSearch(object){
+      if(object.files){
+        for(var i=0; i < object.files.length; i++){
+          if(typeof object.files[i] == 'string'){
+            filesArr.push(object.files[i]);
+          }else if (typeof object.files[i] == 'object'){
+            recSearch(object.files[i]);
+          }
+        }
+      }
+    }
+    // Produce results
+    dirSearch(data, dirName);
+    return filesArr;
   },
 
   permute: function(arr) {
