d29eb650670c970e9d878fb75d80d8815593acf7
robertmackness <robert.mackness.gbr@gmail.com>
2016-03-25 18:25:17 +0000

    Add completed REGEX tests

--- app/regex.js
+++ app/regex.js
@@ -2,25 +2,27 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.regexAnswers = {
   containsNumber : function(str) {
-
+    return !!str.match(/[0-9]/);
   },
 
   containsRepeatingLetter : function(str) {
-
+    return !!str.match(/([a-zA-Z])\1{1,}/);
   },
 
   endsWithVowel : function(str) {
-
+    return !!str.match(/[aeiouAEIOU]$/);
   },
 
   captureThreeNumbers : function(str) {
-
+    var result = str.match(/\d{3}/);
+    if(result) return result[0];
+    else return false;
   },
 
   matchesPattern : function(str) {
-
+    return !!str.match(/^\d{3}\-\d{3}\-\d{4}$/);
   },
   isUSD : function(str) {
-
+    return !!str.match(/^\$(\d{1,3})+(,\d{3})*(\.\d{2})?$/);
   }
 };
