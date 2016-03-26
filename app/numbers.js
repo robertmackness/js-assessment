308ad6fbc452c05173622c51d467063bd3e04341
robertmackness <robert.mackness.gbr@gmail.com>
2016-03-26 18:14:37 +0000

    Improve number to binary function

--- app/numbers.js
+++ app/numbers.js
@@ -2,7 +2,22 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-    return 1 & (num >> bit-1)
+    return (1 & (num >> bit-1));
+
+    // the bitwise & operator returns a 1 in positions that both
+    // evaluated arguments contain a 1. In this case the following
+    // is evaluated:
+    // 00000001 & num(shifted to the right by so many bits)
+    // so if we had the number 4 as num and wanted to know if bit 3 was 1
+    // or 0, the following takes places
+    // 00000001 & (00000100 shifted 3-1 bits (we don't want to shift the evaluated bit off)
+    // 00000001 & 00000001 or more clearly 00000001
+    //                                     00000001
+    //                                  =  00000001  returns 1
+    // If we shifted it 3-2 bits we'd get  00000001
+    //                                     00000010
+    //                                  =  00000000  returns 0
+
   },
 
   base10: function(str) {
@@ -10,11 +25,12 @@ exports.numbersAnswers = {
   },
 
   convertToBinary: function(num) {
-    var binary =  num.toString(2);
-    while(binary.length < 8){
-      binary = "0" + binary;
+    
+    var binary = [];
+    for(var i = 0; i<8; i++){
+      binary.unshift(1 & (num >> i));
     }
-    return binary;
+    return binary.join('');
   },
 
   multiply: function(a, b) {
